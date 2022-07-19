0a2cff2559becc063643b037c497c391fd4f422e
Dandy Nguyễn <62740153+hung2707@users.noreply.github.com>
2022-07-19 08:34:53 +0700

    nuxtjs

---
+++ nuxtjs/pages/classified.vue
@@ -0,0 +1,11 @@
+<template>
+  <Classified/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ClassifiedPage'
+})
+</script>
---
+++ nuxtjs/pages/tin-tuc/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <News/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NewsPage'
+})
+</script>
